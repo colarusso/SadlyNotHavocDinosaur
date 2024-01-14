0877912dd85b22ed0b3f04a0a084113550422d5a
David Colarusso <colarusso@users.noreply.github.com>
2024-01-14 00:17:44 -0500

    file handling updates

--- js/functions.js
+++ js/functions.js
@@ -38,6 +38,66 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });  
 
+
+function saveTextAsFile(tosave,name) {
+    // Handle vendor prefixes.
+    window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
+  
+    // tosave = ID of textarea to save
+    // name = name to save file as, including file extension     
+    // grab the content of the form field and place it into a variable
+    var textToWrite = tosave //document.getElementById(tosave).value;
+    //  create a new Blob (html5 magic) that conatins the data from your form feild
+    var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+        
+    // Specify the name of the file to be saved
+    var fileNameToSaveAs = name;
+        
+    // Optionally allow the user to choose a file name by providing 
+    // an imput field in the HTML and using the collected data here
+    // var fileNameToSaveAs = txtFileName.text;
+  
+    // create a link for our script to 'click'
+    var downloadLink = document.createElement("a");
+    // supply the name of the file (from the var above).
+    // you could create the name here but using a var
+    // allows more flexability later.
+    downloadLink.download = fileNameToSaveAs;
+    // provide text for the link. This will be hidden so you
+    // can actually use anything you want.
+    downloadLink.innerHTML = "My Hidden Link";
+        
+    // allow our code to work in webkit & Gecko based browsers
+    // without the need for a if / else block.
+    window.URL = window.URL || window.webkitURL;
+            
+    // Create the link Object.
+    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    // when link is clicked call a function to remove it from
+    // the DOM in case user wants to save a second file.
+    downloadLink.onclick = destroyClickedElement;
+    // make sure the link is hidden.
+    downloadLink.style.display = "none";
+    // add the link to the DOM
+    document.body.appendChild(downloadLink);
+        
+    // click the new link
+    downloadLink.click();
+}
+  
+function destroyClickedElement(event) {
+    // remove the link from the DOM
+    document.body.removeChild(event.target);
+}
+
+function loadFile(filePath) {
+    return fetch(filePath)
+    .then(response => response.text())
+    .then(data => {
+        return data
+    })
+};
+
 const previews_arr = [
     [
         `\n<div class="preview" onClick="window.location='projects/#litprompts'">
