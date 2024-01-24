5cdf4f4a64e29666b1c59aa7f0414bac2634b163
David Colarusso <colarusso@users.noreply.github.com>
2024-01-24 15:11:52 -0500

    header revert

--- js/functions.js
+++ js/functions.js
@@ -134,9 +134,9 @@ function collapse_setup() {
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    var msg = `David's lab is hosting a <a href="https://suffolklitlab.org/LITCon/2024/" target="_blank">hybrid event @ Suffolk Law</a> on April 8th focusing on practical AI in law. Join us!`
-    document.getElementById('msg_bar').innerHTML = msg;
-    document.getElementById('msg_bar').style.display='block';
+    //var msg = `David's lab is hosting a <a href="https://suffolklitlab.org/LITCon/2024/" target="_blank">hybrid event @ Suffolk Law</a> on April 8th focusing on practical AI in law. Join us!`
+    //document.getElementById('msg_bar').innerHTML = msg;
+    //document.getElementById('msg_bar').style.display='block';
 
     if (!window.location.hash) { 
         try {
