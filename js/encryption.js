40accff2e9fc25ce03f27cabeb21d4582d923d4c
David Colarusso <colarusso@users.noreply.github.com>
2024-02-16 11:04:38 -0500

    function update

--- js/encryption.js
+++ js/encryption.js
@@ -98,7 +98,7 @@
 For exported files:
 Add the following to the head before the declared js with the right relative address
 
-<script src="../../js/encryption.js?v=2024-02-16b"></script>
+<script src="../../js/encryption.js?v=2024-02-16c"></script>
 
 Then, add edit the declared js to read like follows
 
