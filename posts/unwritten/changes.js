20c8188a2a0f63a9cdd3e8d73c78ee088fef9cb6
David Colarusso <colarusso@users.noreply.github.com>
2024-03-23 21:58:58 -0400

    updates

--- posts/unwritten/changes.js
+++ posts/unwritten/changes.js
@@ -2,4 +2,38 @@
 if ((!question_arry[question]) || (question.match(/\*}}$/))) {
 
 //openai_call(llm_prompt)
-openai_call(llm_prompt.substring(llm_prompt.length - 16000*4))
+openai_call(llm_prompt.substring(llm_prompt.length - 15000*4))
+
+// Add after restartButton listener
+document.getElementById("restartButton_companion").addEventListener("click", function() {
+    location.reload();
+});
+
+// Add after removing hard coded style on divs
+#send {
+    float:left;
+    width:75%;
+    margin: 5px 0 0px 0;
+  }
+  
+// Add
+
+<button id="restartButton_companion">Restart</button> // after the send buttons
+
+#restartButton_companion {
+    float:right;
+    width:24%;
+    margin: 5px 0 10px 0;
+}
+
+button {
+	-webkit-appearance:none;
+	opacity: 1;
+	border-radius: 3px;
+	color:black;
+	border: 1px solid #7b7b7b;
+	padding: 3px;
+	background: rgb(211, 211, 211);
+}
+
+
